1c3ae611366d672ae96dded85a7fd9474c8d55bb
NAVDEESH <navdeesh8569.beaift25@chitkara.edu.in>
2025-08-25 22:19:22 -0700

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,43 @@
+# arithmetic operator
+# a=10
+# b=20
+# c=a+b
+# print(c)  sum
+
+# a=10
+# b=20
+# c=b-a
+# print(c) subtract
+
+# a=10
+# b=20
+# c=b/a
+# print(c)     divide
+
+# a=10
+# b=20
+# c=b%a
+# print(c)        remainder
+
+# a=10
+# a+=10
+# print(a)
+
+a=int(input("enter number"))
+b=int(input("enter number"))
+c=int(input("enter number"))
+print(a>b and b>c)
+
+
+# a=int(input("enter number"))
+# b=int(input("enter number"))
+# c=int(input("enter number"))
+# print(a>b or b>c)
+
+
+
+# a=int(input("enter number"))
+# b=int(input("enter number"))
+# print(not a>b)
+
+
