2f9812bd49818d90066f553a2ba84a940b33506f
NAVDEESH <navdeesh8569.beaift25@chitkara.edu.in>
2025-08-25 22:17:51 -0700

    Add files via upload

---
+++ day3hw.py
@@ -0,0 +1,34 @@
+# for i in range(1,11):
+#     print("first 10 natural number",i)
+# for i in range(1,101):
+#     if i%2==0:
+#      print("even numbers",i)
+# number=5
+# for i in range(1,11):
+#     product=number*i
+#     print(product)
+# for i in range(1,11):
+#     print("square of number are",i**2)
+
+# num=int(input("enter number"))
+# fact=1
+# for i in range(1,num+1):
+#     fact=fact*i
+# print("factorial",num,"is",fact)
+
+a=0
+b=1
+n=int(input())
+print("fibonacci aeries")
+for i in range(n):
+    print(a)
+    a,b=b,a+b
+
+
+
+
+
+
+
+ 
+
